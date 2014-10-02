751fd2f78dc8a5ae41a2ad804e703d55bcaca3a9
szllong123 <1546221417@qq.com>
2014-10-03 00:53:31 +0800

    fix some fault, rectify deal with loop, this is not an understandable point

--- HD_number_sequence1005.c
+++ HD_number_sequence1005.c
@@ -14,18 +14,25 @@ int main(int argc, char *argv[])
 {
 	int first_num, second_num, third_num;
 	int A, B;
-	int i, n;
+	int i;
+	long n;
+	int temp;
 	while(1){
-		scanf("%d %d %d", &A, &B, &n);
-		if(A == 0 && B == 0 && n == 0)
-			break;
+		scanf("%d %d %d", &A, &B, &temp);
+		if(A == 0 && B == 0 && temp == 0)
+			break;	
+		n = temp % 49;
+
 		first_num = 1, second_num = 1, third_num = 0;
 		for(i = 2; i < n; i++){
 			third_num = (A * second_num + B * first_num) % 7;
 			first_num = second_num;
 			second_num = third_num;
 		}
-		printf("%d\n", third_num);
+		if(n == 1 || n == 2)
+			printf("1\n");
+		else
+			printf("%d\n", third_num);
 	}
 	return 0;
 }
